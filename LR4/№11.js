9901aee5b75b6ed8b569b3a109c1b2550fa9b6b6
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-12-02 12:01:01 +0300

    11 Задание

---
+++ LR4/№11.js
@@ -0,0 +1,2 @@
+obj =  {ru:['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'], en:['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',  'Saturday', 'Sunday']}
+console.log(obj.ru[0], obj.en[2])
