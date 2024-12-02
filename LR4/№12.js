073d7dd11df7d3e1899b96675962cedc83377019
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-12-02 12:01:26 +0300

    12 Задание

---
+++ LR4/№12.js
@@ -0,0 +1,3 @@
+obj =  {ru:['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'], en:['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',  'Saturday', 'Sunday']}
+lang = 'ru' , day = 3
+console.log(obj[lang][day])
