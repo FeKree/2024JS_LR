580b97ce552a630a16a49f1a7b64e6993e2d589b
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-12-06 17:33:05 +0300

    7 Задание

--- LR5/№7.js
+++ LR5/№7.js
@@ -1,1 +1,22 @@
-
+flag = false
+function m(s)
+{
+  for(i =0; i < s.length; i++)
+  {
+    if(s[i]===s[i+1])
+    {
+      flag = true
+      return flag
+      break
+    }
+  }
+}
+a = (m([3,5,5,7,8,4,9]))
+if(a === true)
+{
+  console.log('В массиве есть повторные элементы подряд')
+}
+else
+{
+  console.log('В массиве нет повторных элементов подряд')
+}
