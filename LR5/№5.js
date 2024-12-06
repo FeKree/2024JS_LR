47db1015a07c3edb1ac9cdbeeae72eadd9b26511
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-12-06 14:27:37 +0300

    5 Задание

--- LR5/№5.js
+++ LR5/№5.js
@@ -1,1 +1,20 @@
-
+var flag = false
+function is_five(num)
+{
+    for(var i = 0; i<num.length;i++)
+    {
+    if (num[i]==5)
+    {
+      flag = true
+      return flag
+    }
+    }
+}
+if (is_five([3,3,3,4,6,5]) == true)
+{
+    console.log('Присутствует')
+}
+else
+{
+    console.log('Нету')
+}
