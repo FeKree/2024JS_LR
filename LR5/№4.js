9a91a66168dea24e84dd3a77f1a1a023daca33ad
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-12-06 14:15:49 +0300

    4 Задание

--- LR5/№4.js
+++ LR5/№4.js
@@ -1,1 +1,31 @@
-
+function kvadrat(r)
+{
+    switch(r)
+    {
+        case 1:
+        return "Понедельник"
+        break
+        case 2:
+        return "Вторник"
+        break
+        case 3:
+        return "Среда"
+        break
+        case 4:
+        return "Четверг"
+        break
+        case 5:
+        return "Пятница"
+        break
+        case 6:
+        return "Суббота"
+        break
+        case 7:
+        return "Воскресенье"
+        break
+        default:
+        return "От 1 до 7 включительно"
+        break
+    }    
+}
+console.log(kvadrat(5))
